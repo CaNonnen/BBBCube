1b518d6160e1ac6173e9558e5ad1ffe9b22add1d
EML-PC-Einrad <haan1047@h-ka.de>
2025-11-13 16:51:33 +0100

    Multithreading HelloWorld

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <pthread.h>
+#include <cstdlib>
+#include <unistd.h>
 
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl;
-	return 0;
+void* thread1main(void* arg);
+void* thread2main(void* arg);
+
+int main()
+{
+	pthread_t thread1_id;
+	pthread_t thread2_id;
+	int ret = pthread_create(&thread1_id, NULL, thread1main, NULL);
+	if(ret < 0)
+	{
+		cerr << "pthread_create 1 failed!" << endl;
+	}
+
+	ret = pthread_create(&thread2_id, NULL, thread2main, NULL);
+	if(ret < 0)
+	{
+		cerr << "pthread_create 2 failed!" << endl;
+	}
+
+
+	pthread_join(thread1_id, NULL);
+	pthread_join(thread2_id, NULL);
+}
+
+void* thread1main(void* arg)
+{
+	while(true)
+	{
+		cout << "Hello World! Ich bin der erste Thread!" << endl;
+		//sleep(1);
+	}
+}
+void* thread2main(void* arg)
+{
+	while(true)
+	{
+		cout << "Hello World! Ich bin der zweite Thread!" << endl;
+		//sleep(1);
+	}
 }
